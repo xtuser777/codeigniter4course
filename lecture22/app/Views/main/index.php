2ef4b34035b24d0fdd3aec0a89666b33f428c4da
Lucas Oliveira <lucaoxt@gmail.com>
2024-04-10 14:56:46 -0300

    Lecture 22 - Assets

---
+++ lecture22/app/Views/main/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture 22 - Assets</title>
+    <link rel="stylesheet" href="<?=base_url("assets/css/bootstrap.min.css")?>">
+</head>
+<body>
+    <h3>Página</h3>
+
+    <img src="<?=base_url("assets/images/world.jpeg")?>" alt="World">
+</body>
+</html>
