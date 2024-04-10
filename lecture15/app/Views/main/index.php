4ab1b4fdedc622f8ac791d9b526ca82e82f7ab00
Lucas Oliveira <lucaoxt@gmail.com>
2024-04-09 21:43:18 -0300

    Lecture 15 - Layout sections

---
+++ lecture15/app/Views/main/index.php
@@ -0,0 +1,9 @@
+<?= $this->extend("layout/default") ?>
+
+<?= $this->section("section1") ?>
+<p>Conteúdo da seção 1</p>
+<?= $this->endSection() ?>
+
+<?= $this->section("section2") ?>
+<p>Conteúdo da seção 2</p>
+<?= $this->endSection() ?>
